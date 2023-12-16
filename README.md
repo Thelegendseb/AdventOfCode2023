a3cf9e3b8ac840c23dce229d1e695b6cee053d0b
SebCl <c3033815@newcastle.ac.uk>
2023-12-16 01:06:01 +0000

    Changed main.py and README.md to account for the fact that there is 2 parts to each day

--- README.md
+++ README.md
@@ -7,17 +7,17 @@ This directory contains my implementations for the Advent of Code 2023
 1. Make sure you have Python installed on your system.
 2. Navigate to the project directory in the terminal.
 
-### Running a Specific Day's Solution
+### Running a Specific Day's Solution and Part
 
 ```bash
-python main.py <day_number>
+python main.py <day> <part>
 ```
 Replace <day_number> with the day you want to run.
-
+Replace <dpart> with the part of the challenge you want to run.
 ### Example
 ```bash
-python main.py 1
+python main.py 1 1
 ```
-This will execute the solution for day 1.
+This will execute the solution for day 1 part 1.
 
 Merry Christmas all! 🎄
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import sys
 import importlib
 
-def run_day(day):
+def run_part(day, part):
     try:
         module = importlib.import_module(f'Day{day}.day{day}')
-        # Run the module without calling a specific function
-        return module
+        part_function = getattr(module, f'part{part}', None)
+
+        if part_function:
+            part_function()
+        else:
+            print(f"Part {part} not found for Day {day}.")
     except ModuleNotFoundError:
         print(f"Day {day} not found or not implemented yet.")
-        return None
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <day>")
-    else:
-        day_to_run = sys.argv[1]
-        module = run_day(day_to_run)
-        # if module:
-        #     print(f"Running day {day_to_run}")
-        #     # This will execute the module's code without calling a specific function
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <day> <part>")
+        sys.exit(1)
+
+    day_to_run = sys.argv[1]
+    part_to_run = sys.argv[2]
+
+    run_part(day_to_run, part_to_run)
