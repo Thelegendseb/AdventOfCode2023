7d6885d2d26d6f6c5f0c273fa05bdc99e26a9345
SebCl <c3033815@newcastle.ac.uk>
2023-12-17 07:29:13 +0000

    Day 4 completed! 🎄

--- Day4/day4.py
+++ Day4/day4.py
@@ -6,4 +6,72 @@ def part1():
     sum = 0
 
     for line in input:
-        print(line)
+
+        points = 0
+
+        parts = line.split(" | ")
+        winning_parts = parts[0].split(": ")
+        winning_numbers_str = winning_parts[1]
+        numbers_str = parts[1]
+
+        numbers = list(map(int, filter(None, numbers_str.split(' '))))
+        winning_numbers = list(map(int, filter(None, winning_numbers_str.split(' '))))
+
+        for num in numbers:
+            if num in winning_numbers:
+                if points == 0:
+                    points = 1
+                else:
+                    points *= 2
+
+        sum += points
+
+    print(sum)
+
+def part2():
+    input = read_input(4)
+
+    copies = {}
+    # card number: copy count
+
+    for line in input:
+
+        matches = 0
+
+        parts = line.split(" | ")
+        winning_parts = parts[0].split(": ")
+        card_num = int(winning_parts[0].rsplit(' ', 1)[-1]) if winning_parts[0] else None
+        winning_numbers_str = winning_parts[1]
+        numbers_str = parts[1]
+
+        numbers = list(map(int, filter(None, numbers_str.split(' '))))
+        winning_numbers = list(map(int, filter(None, winning_numbers_str.split(' '))))
+
+        if card_num in copies:
+            copies[card_num] += 1
+        else:
+            copies[card_num] = 1
+
+        for num in numbers:
+            if num in winning_numbers:
+                matches += 1
+
+        for i in range(matches):
+            key = i + 1 + card_num
+            if key in copies:
+                copies[key] += 1
+            else:
+                copies[key] = 1
+
+        if copies[card_num] > 1:
+            for c in range(copies[card_num] - 1):
+                for i in range(matches):
+                    key = i + 1 + card_num
+                    if key in copies:
+                        copies[key] += 1
+                    else:
+                        copies[key] = 1
+
+    cardsum = sum(copies.values())
+
+    print(cardsum)
